ee05eacdd3b54c2a798b21a2b9b8851861205a15
Mario <mario10rafael@gmail.com>
2026-03-10 08:10:39 +0200

    made start game form repsonsive

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,18 +1,27 @@
 import "./App.css";
 
+import { useState } from "react";
 import GameOverModal from "./components/GameOverModal";
 import MouseIndicator from "./components/MouseIndicator";
 import usePointerPosition from "./hooks/usePointerPosition";
 import useTicTacToe from "./hooks/useTicTactoe";
+import StartGameForm from "./components/StartGameForm";
 
 function App() {
-  const { makeMove, ticTacToe, modalRef, nextMove, winner, restartGame } =
-    useTicTacToe();
+  const {
+    makeMove,
+    ticTacToe,
+    modalRef: gameOverModalRef,
+    nextMove,
+    winner,
+    restartGame,
+  } = useTicTacToe();
+  const [username, setUsername] = useState<string>("");
   const pointerPosition = usePointerPosition();
 
   return (
     <>
-      <MouseIndicator pointerPosition={pointerPosition} label="Mario" />
+      {/* <MouseIndicator pointerPosition={pointerPosition} label="Mario" /> */}
       <main>
         <h1 className="title">
           <span className="tic">Tic</span>
@@ -20,33 +29,37 @@ function App() {
           <span className="toe">Toe</span>
         </h1>
 
-        <div className="game-wrapper">
-          <h2 style={{ textAlign: nextMove == "X" ? "start" : "end" }}>
-            {nextMove} to move
-          </h2>
-          <div className="tic-tac-toe">
-            {ticTacToe.map((row, i) => (
-              <>
-                {row.map((cell, j) => (
-                  <button
-                    className={`cell ${cell == 0 && "empty"}`}
-                    key={i * j + j}
-                    onClick={
-                      cell == 0
-                        ? () => makeMove({ cell_i: i, cell_j: j })
-                        : () => {}
-                    }
-                  >
-                    {cell != 0 && cell}
-                  </button>
-                ))}
-              </>
-            ))}
+        {username ? (
+          <div className="game-wrapper">
+            <h2 style={{ textAlign: nextMove == "X" ? "start" : "end" }}>
+              {nextMove} to move
+            </h2>
+            <div className="tic-tac-toe">
+              {ticTacToe.map((row, i) => (
+                <>
+                  {row.map((cell, j) => (
+                    <button
+                      className={`cell ${cell == 0 && "empty"}`}
+                      key={i * j + j}
+                      onClick={
+                        cell == 0
+                          ? () => makeMove({ cell_i: i, cell_j: j })
+                          : () => {}
+                      }
+                    >
+                      {cell != 0 && cell}
+                    </button>
+                  ))}
+                </>
+              ))}
+            </div>
           </div>
-        </div>
+        ) : (
+          <StartGameForm />
+        )}
         <GameOverModal
           winner={winner}
-          modalRef={modalRef}
+          modalRef={gameOverModalRef}
           onPlayAgain={restartGame}
         />
       </main>
